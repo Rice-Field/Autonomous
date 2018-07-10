5db6c54f8b1ca258efe98d25f427aad41657bddc
Jonathon Rice <jonrice@knights.ucf.edu>
2018-07-10 15:41:43 -0400

    Add files via upload

--- Courses/Artificial Intelligence for Robotics/kalman.py
+++ Courses/Artificial Intelligence for Robotics/kalman.py
@@ -1,5 +1,12 @@
 # Artificial Intelligence for Robotics, Udacity
 # Jonathon Rice
 
+import math
 import numpy as np
 
+# Calculates gaussian values
+def gaussian(mu, sigma, x):
+	n = pow(np.e, ((x-mu)**2)/(-2*sigma*sigma))
+	return (n/(np.sqrt(2*np.pi)*sigma))
+
+print(gaussian(10, 2, 8))
